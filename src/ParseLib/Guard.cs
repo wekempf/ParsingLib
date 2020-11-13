0c1f9b0d52845ddce6ea47e94502578d3385addf
William Kempf <bill.kempf@salliemae.com>
2020-11-13 10:09:52 -0500

    Initial

---
+++ src/ParseLib/Guard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParseLib
+{
+    internal class Guard
+    {
+        private Guard()
+        {
+        }
+
+        public static Guard Against { get; } = new Guard();
+
+        public T Null<T>(T? value, string paramName)
+            where T : class
+            => value ?? throw new ArgumentNullException(paramName);
+
+        public T Range<T>(T value, T min, T max, string paramName)
+            where T : IComparable<T>
+            => value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0
+                    ? value
+                    : throw new ArgumentOutOfRangeException(paramName);
+    }
+}
